6d89d29b9054e52a4d1c8dad71af8846376a30c6
NaziaSiddique <nazia.siddique04@gmail.com>
2024-05-05 10:35:38 +0100

    Added mock to test

--- src/test/java/org/Restaurant/OrderTest.java
+++ src/test/java/org/Restaurant/OrderTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class OrderTest {
 
@@ -13,12 +14,22 @@ public class OrderTest {
     @Before
     public void initOrder(){
         ArrayList<Meal> meals = new ArrayList<>();
-        Meal meal1 = new Meal("Chicken Tikka Baguette", "Fresh Chicken with Special Sauce", 9.99);
-        Meal meal3 = new Meal("Apna Style Pizza", "Special sauce for base and fresh chicken and mince", 15.99);
+//        Meal meal1 = new Meal("Chicken Tikka Baguette", "Fresh Chicken with Special Sauce", 9.99);
+//        Meal meal3 = new Meal("Apna Style Pizza", "Special sauce for base and fresh chicken and mince", 15.99);
+//        meals.add(meal1);
+//        meals.add(meal3);
+
+        Meal meal1 = mock(Meal.class);
+        when(meal1.getPrice()).thenReturn(9.99);
+
+        Meal meal2 = mock(Meal.class);
+        when(meal2.getPrice()).thenReturn(15.99);
+
         meals.add(meal1);
-        meals.add(meal3);
+        meals.add(meal2);
         order = new Order(meals);
     }
+
 
     @Test
     public void calculateTotalPrice() {
